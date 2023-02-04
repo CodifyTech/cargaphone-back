177c4dc296750444dc8f74178a23dc3c9f32ff94
Everton <92490650+HeyEverton@users.noreply.github.com>
2023-02-04 18:51:45 -0300

    factories criadas e funcionando

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Estabelecimento;
+use App\Models\Totem;
 use App\Models\Unidade;
 use App\Models\User;
 use Database\Factories\UserFactory;
@@ -20,5 +22,6 @@ class DatabaseSeeder extends Seeder
     {
         Unidade::factory()->create();
         User::factory()->create();
+        Totem::factory()->create();
     }
 }
