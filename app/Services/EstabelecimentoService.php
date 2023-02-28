2417a2a687188c95a1e7b287716869db506a3069
Everton <92490650+HeyEverton@users.noreply.github.com>
2023-02-28 11:15:04 -0300

    correcao code smell do bot

--- app/Services/EstabelecimentoService.php
+++ app/Services/EstabelecimentoService.php
@@ -10,7 +10,9 @@ class EstabelecimentoService
 {
     public function create(array $data)
     {
-        if ($this->existeCnpj($data['cnpj'])) return '403';
+        if ($this->existeCnpj($data['cnpj'])) {
+            return '403';
+        }
 
         $payload = Token::decode();
         $data['tenant_id'] = $payload['tenant_id'];
